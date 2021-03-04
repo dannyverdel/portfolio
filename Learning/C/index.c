b983e6b8ee39c539a6b9eb62bb8a1ccbe5bbe4a9
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-04 20:09:12 +0100

    Continued

--- Learning/C/index.c
+++ Learning/C/index.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
 
-void *f(void)
-{
-    static int array[5] = {2, 3, 5, 7, 11};
-
-    return(array);
-}
+void modify(char a[]);
 
 int main()
 {
-    int x;
-    int *array;
+    //get text from input
+    char name[64];
+    fgets(name, 64, stdin);
 
-    array = f();
-    for(x = 0; x < 5; x++)
-    {
-        printf("%2d\n", array[x]);
-    }
+    //send text to function
+    modify(name);
+
+    //display the modified string
+    printf("%s", name);
 
     return(0);
+}
+
+void modify(char *a)
+{
+    //in the function convert all letters to uppercase and all spaces to underlines
+    while(*a)
+    {
+        *a = toupper(*a);
+        if(*a == ' ')
+        {
+            *a = '_';
+        }
+        a++;
+    }
 }
