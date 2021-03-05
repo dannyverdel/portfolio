6055e32501d9c6eecf61d94c5abe84c9c2b30308
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-05 21:11:57 +0100

    Continued with learning

--- Learning/C/index.c
+++ Learning/C/index.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <string.h>
-#include <ctype.h>
-
-void modify(char a[]);
 
 int main()
 {
-    //get text from input
-    char name[64];
-    fgets(name, 64, stdin);
+    struct weather {
+        char day[10];
+        float temp;
+    };
+    struct weather week[7] = {
+        {"Sunday", 65.5},
+        {"Monday", 34.5},
+        {"Tuesday", 86.3},
+        {"Wednesday", 71.8},
+        {"Thursday", 79.4},
+        {"Friday", 75.2},
+        {"Saturday", 72.5}
+    };
 
-    //send text to function
-    modify(name);
+    struct weather temporary;
+    int x, outer, inner;
 
-    //display the modified string
-    printf("%s", name);
+    puts("This week's unorganized forecast:");
+    for(x = 0; x < 7; x++)
+    {
+        printf("%10s %.1f degrees\n", week[x].day, week[x].temp);
+    }
+    putchar('\n');
+
+    for(outer = 0; outer < 7; outer++)
+    {
+        for(inner = outer + 1; inner < 7; inner++)
+        {
+            if(week[outer].temp > week[inner].temp)
+            {
+                temporary = week[inner];
+                week[inner] = week[outer];
+                week[outer] = temporary;
+            }
+        }
+    }
+
+    puts("This week's organized forecast:");
+    for(x = 0; x < 7; x++)
+    {
+        printf("%10s %.1f degrees\n", week[x].day, week[x].temp);
+    }
+    putchar('\n');
 
     return(0);
-}
-
-void modify(char *a)
-{
-    //in the function convert all letters to uppercase and all spaces to underlines
-    while(*a)
-    {
-        *a = toupper(*a);
-        if(*a == ' ')
-        {
-            *a = '_';
-        }
-        a++;
-    }
 }
