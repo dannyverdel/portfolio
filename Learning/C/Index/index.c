0e58e85833a57a4ca098d7011e9dab1f8b79ba3a
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-09 15:09:46 +0100

    Continued with course

--- Learning/C/Index/index.c
+++ Learning/C/Index/index.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    struct person {
-        char name[64];
-        int age;
-    };
-    struct person danny = {
-        "Danny Verdel",
-        18
-    };
 
-    printf("My name is %s and I am %d years old!\n", danny.name, danny.age);
 
     return(0);
 }
