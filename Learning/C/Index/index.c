2231e1d0675e409a9dd014d451b8f8f7c234086c
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-10 19:23:10 +0100

    Continued learning

--- Learning/C/Index/index.c
+++ Learning/C/Index/index.c
@@ -1,18 +1,55 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <string.h>
 
 int main()
 {
-    char name[] = "Danny Verdel";
-    int a;
+    //Fetch input
+    /*
+    receive name
+    write a loop to process single character input, storing it in a buffer called name[]
+    The buffer cna hold up to 32 characters
+    */
+    //Process input and a string literal to generate a phrase
+    /*
+    copy input into the buffer array
+    append text from the last array into the buffer
+    last array contains string literal "Passed the challenge"
+    */
+    //Output the phrase
+    /*
+    output the buffer array
+    no puts or printf
+    use putchar() in a loop
+    */
+
+    /*
+    what is your name? Danny
+    Danny passed the test
+    */
+
+    char name[32];
+    char buffer[64];
+    char last[] = "passed the challenge";
+    int ch, a;
+
+    printf("What is your name? ");
+
+    while((ch=getchar())!='\n')
+    {
+        name[a] = ch;
+        a++;
+        if(a==31)
+            break;
+    }
+    name[a] = '\0';
+
+    strcpy(buffer, name);
+    strcat(buffer, " ");
+    strcat(buffer, last);
 
     a = 0;
 
-    while(name[a]!='\0')
-    {
-        putchar(name[a]);
-        a++;
-    }
+    while(putchar(buffer[a++]));
 
     return(0);
 }
