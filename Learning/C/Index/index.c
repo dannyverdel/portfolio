c7973e6fbc24923942190066065b347802bf685c
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-11 12:12:15 +0100

    Continued learning

--- Learning/C/Index/index.c
+++ Learning/C/Index/index.c
@@ -1,55 +1,19 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    //Fetch input
-    /*
-    receive name
-    write a loop to process single character input, storing it in a buffer called name[]
-    The buffer cna hold up to 32 characters
-    */
-    //Process input and a string literal to generate a phrase
-    /*
-    copy input into the buffer array
-    append text from the last array into the buffer
-    last array contains string literal "Passed the challenge"
-    */
-    //Output the phrase
-    /*
-    output the buffer array
-    no puts or printf
-    use putchar() in a loop
-    */
+    char *buffer;
 
-    /*
-    what is your name? Danny
-    Danny passed the test
-    */
-
-    char name[32];
-    char buffer[64];
-    char last[] = "passed the challenge";
-    int ch, a;
-
-    printf("What is your name? ");
-
-    while((ch=getchar())!='\n')
+    buffer = (char *)malloc(sizeof(char)*128);
+    if(buffer=NULL)
     {
-        name[a] = ch;
-        a++;
-        if(a==31)
-            break;
+        puts("Unable to allocate memory");
+        exit(1);
     }
-    name[a] = '\0';
-
-    strcpy(buffer, name);
-    strcat(buffer, " ");
-    strcat(buffer, last);
-
-    a = 0;
-
-    while(putchar(buffer[a++]));
+    puts("Buffer allocated");
+    free(buffer);
+    puts("Buffer freed");
 
     return(0);
 }
