2e5e39d366e7120208d4d84aaca9fdb46d7b486b
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-11 20:20:20 +0100

    Continued learning

--- Learning/C/Index/index.c
+++ Learning/C/Index/index.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    char *buffer;
+    char *fruit[10];
+    char buffer[32];
+    int x;
 
-    buffer = (char *)malloc(sizeof(char)*128);
-    if(buffer=NULL)
+    for(x=0;x<10;x++)
     {
-        puts("Unable to allocate memory");
-        exit(1);
+        printf("Enter fruit #%d: ",x+1);
+        scanf("%s", &buffer);
+
+        fruit[x] = (char *)malloc(strlen(buffer)+1);
+        if(fruit[x]==NULL)
+        {
+            puts("Memory allocation failed");
+            exit(1);
+        }
+
+        strcpy(fruit[x],buffer);
     }
-    puts("Buffer allocated");
-    free(buffer);
-    puts("Buffer freed");
+
+    for(x=0;x<10;x++)
+        printf("%s\n",fruit[x]);
 
     return(0);
 }
