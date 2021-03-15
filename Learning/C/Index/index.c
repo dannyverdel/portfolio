45af571f5b8128f740c2e3bca9362b7a210facfc
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-15 12:23:04 +0100

    Continued with learning

--- Learning/C/Index/index.c
+++ Learning/C/Index/index.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
 
 int main()
 {
-    char path[512];
+    //Prompt the user for a pathname
+    char input;
+    char path[100];
+    int x;
 
-    getcwd(path, 512);
-    printf("This program is running in the %s directory\n", path);
+    printf("Specify a filepath: ");
+
+    x=0;
+    while(input != '\n')
+    {
+        input = getchar();
+        putchar(input);
+        path[x] = input;
+        x++;
+    }
+
+    /*
+    for(x=0;x<strlen(path);x++)
+    {
+        if(path[x] == '\\')
+            path[x] == "\\";
+    }
+    */
+
+    //Open that directory and read its files
+    DIR *dir;
+    struct dirent *file;
+
+    dir = opendir(path);
+    if(dir==NULL)
+    {
+        puts("Unable to open this directory");
+        return(1);
+    }
+
+    while((file=readdir(dir)) != NULL)
+        printf("Found the file %s\n",file->d_name);
+
+    closedir(dir);
+
+    //Output the filenames to filelist.txt
 
     return(0);
 }
