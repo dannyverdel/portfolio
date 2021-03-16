b8bef3f991d66b3f92f95a26f23e50818194dda0
dannyverdel <danny.verdel@dobbetransport.nl>
2021-03-16 20:05:51 +0100

    Continued learning

---
+++ Learning/C/Index/cmake-build-debug/CMakeFiles/Index.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Index.dir/main.c.obj"
+  "Index.exe"
+  "Index.exe.manifest"
+  "Index.pdb"
+  "libIndex.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Index.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
